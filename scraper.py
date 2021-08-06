4f4be1725578be35aba39349a989c632fa65ec22
Christian Abueg <cja2400@gmail.com>
2021-08-06 10:47:55 -0700

    Can webscrape the name and namesake from the Jojo wiki

--- scraper.py
+++ scraper.py
@@ -2,13 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 
 response = requests.get(
-    url= "https://jojo.fandom.com/wiki/Will_Anthonio_Zeppeli"
+    url= "https://jojo.fandom.com/wiki/Dio_Brando"
 )
 soup = BeautifulSoup(response.content, 'html.parser')
 
-character_Name = soup.find(id="firstHeading")
+character_Name = soup.find('h2', attrs={'data-source' : "title"}).string
 
-div = soup.find_all("a", {"class": "extiw"})
 
-print(div)
+div = soup.find('div', attrs={'data-source' : "namesake"})
+namesake = div.find("a", {"class": "extiw"}).string
+
+
+
+
+print(character_Name)
+print(namesake)
+
 
