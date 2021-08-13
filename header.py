3a5878f182c5d89096a1712b345b68efa5e9cdb5
Christian Abueg <cja2400@gmail.com>
2021-08-13 10:50:08 -0700

    Webscraper now adds stand namesakes to list of namesakes

--- header.py
+++ header.py
@@ -1,32 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get(
-    url= "https://jojo.fandom.com/wiki/Jonathan_Joestar"
-)
-soup = BeautifulSoup(response.content, 'html.parser')
 #returns a string of character name
 def find_character_name(soup):
     return soup.find('h2', attrs={'data-source' : "title"}).string
 
-#returns a list of namesakes or none if applicable
-def find_character_namesakes(soup):
-    try:
-        div = soup.find('div', attrs={'data-source' : "namesake"})
-        namesake = div.find_all("a", {"class": "extiw"})
-
-        #list comprehension.Equivalent to 
-        #
-        #listOfNamesakes = []
-        #for a in namesake:
-        #   listOfNamesakes.append(a.string)
-        #
-        #saves a lot of space
-        listOfNamesakes = [a.string for a in namesake]
-        return listOfNamesakes
-
-    except AttributeError:
-        return []
 
 #returns stand name or None if applicable
 def find_character_stand(soup):
@@ -37,10 +15,52 @@ def find_character_stand(soup):
     except AttributeError:
         return None
 
-    
+def find_stand_namesake(soup):
+    div = soup.find('div', attrs={'data-source' : "namesake"})
+    namesake = div.find("a", {"class": "extiw"}).string
+    return namesake 
+
+#returns a list of namesakes or none if applicable
+def find_character_namesakes(soup):
+    try:
+        listOfNamesakes = []
+
+        if(find_character_stand(soup) is not None):
+            div = soup.find('div', attrs={'data-source' : "stand"})
+            stand = div.find("a")
+            response = requests.get(
+                url= "https://jojo.fandom.com" + stand['href'])
+            stand_Page = BeautifulSoup(response.text, "html.parser")
+            stand_Namesake = find_stand_namesake(stand_Page)
+            listOfNamesakes.append(stand_Namesake)
+
+
+        div = soup.find('div', attrs={'data-source' : "namesake"})
+        namesake = div.find_all("a", {"class": "extiw"})
+        
+        for a in namesake:
+            listOfNamesakes.append(a.string)
+
+        return listOfNamesakes
+
+    except AttributeError:
+        if listOfNamesakes: return listOfNamesakes
+        else: return []
 
     
+def get_character_info(link_to_character_page):
+    response = requests.get(
+        url= "https://jojo.fandom.com" + link_to_character_page)
+    character_Page = BeautifulSoup(response.text, "html.parser")
+    character_Name = find_character_name(character_Page)
+    character_Namesake = find_character_namesakes(character_Page)
+    character_Stand = find_character_stand(character_Page)
 
-# print(namesake)
+    print(character_Name)
+    print(character_Namesake)
+    print(character_Stand)
+    print()
+    
+
 
 
--- jojoScraper.py
+++ jojoScraper.py
@@ -1,46 +1,33 @@
 import requests
 from bs4 import BeautifulSoup
-from header import find_character_name, find_character_namesakes, find_character_stand
-import time
+from header import *
+from multiprocessing import Pool
 
-#part = 1 for now since going through 
-part = 1
-
-
-starttime = time.time()
-while(part < 6):
+part = 4
+while(part < 5):
     response = requests.get(
     url= "https://jojo.fandom.com/wiki/Template:Part_"+ str(part) +"_Character_Table"
 )
-    webPage = BeautifulSoup(response.content, "html.parser")
+    webPage = BeautifulSoup(response.text, "html.parser")
     characterTable = webPage.find("table")
 
     #created empty list run a for loop checking for all links in table
     listofLinks = []
+
     for i in characterTable.find_all("a", href=True):
         #checks if element is a picture(starts with https) or if it already exists in list
         if i['href'][0] == "h" or i['href'] in listofLinks:
             pass
+        
         #otherwise adds to list
         else:
             listofLinks.append(i['href'])
+    
     #pop the first element of the list because that is the part name (eg. Phantom Blood, Battle Tendancy)
     listofLinks.pop(0)
 
-    for i in listofLinks:
-        response = requests.get(
-        url= "https://jojo.fandom.com" + i)
+    with Pool(5) as p:
+        records = p.map(get_character_info, listofLinks)
 
-        character_Page = BeautifulSoup(response.content, "html.parser")
-        character_Name = find_character_name(character_Page)
-        character_Namesake = find_character_namesakes(character_Page)
-        character_Stand = find_character_stand(character_Page)
-
-        print(character_Name)
-        print(character_Namesake)
-        print(character_Stand)
-        print()
     
     part += 1
-
-print('That took {} seconds'.format(time.time() - starttime))
